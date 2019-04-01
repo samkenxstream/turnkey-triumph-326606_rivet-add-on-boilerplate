b350458aa29c6ef9e56a26d34a72b0547c10db8f
Rachel <racmocon@gmail.com>
2019-04-01 16:06:08 -0400

    Rebuilt dist folder with correct copyright statement

--- dist/css/rivet-add-on-boilerplate.css
+++ dist/css/rivet-add-on-boilerplate.css
@@ -1,7 +1,7 @@
 /*!
 * rivet-add-on-boilerplate - @version 0.1.1
 
-* Copyright (C) 2019 The Trustees of Indiana University
+* Copyright (C) 2018 The Trustees of Indiana University
 * SPDX-License-Identifier: BSD-3-Clause
 */
 
