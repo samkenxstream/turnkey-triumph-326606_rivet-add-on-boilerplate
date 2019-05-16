f61951c01a4c2f689c8550be4f864cb4a4bdfd8c
Rachel <racmocon@gmail.com>
2019-05-16 12:23:15 -0400

    Flattened js structure

--- src/js/rivet-add-on-boilerplate.js
+++ src/js/rivet-add-on-boilerplate.js
@@ -1,1 +1,15 @@
-export { default as AddOn } from './components/rivet-add-on';
+export class AddOn {
+  constructor(options) {
+    this._handleClick = this._handleClick.bind(this);
+
+    this.init(options);
+  }
+
+  _handleClick() {
+    alert('Button clicked!');
+  }
+
+  init(options) {
+    document.getElementById(options.id).addEventListener('click', this._handleClick, false);
+  }
+}
